dad6a317f5a313a533772ab21784bdec4c1d2cd0
VegetableCat <mail@loli.college>
2018-03-05 18:16:24 +0800

    add unclenoway.py

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
-# unclenoway
+## unclenoway    
+http://118.190.174.130:3101/lostFound/20
+
+https://github.com/zizyzhang/socket.io-client/blob/master/zuul.config.js
+
+## express js
+
+http://unclenoway.com/lost-found/js/main.js
+```
+/**
+ * Created by Zizy on 11/07/2017.
+ */
+```
+确定zizy就是https://github.com/zizyzhang/
+
+## port scan
+```
+─[$] <> nmap -O 118.190.174.130
+
+Starting Nmap 7.25BETA1 ( https://nmap.org ) at 2018-02-07 12:07 CST
+RTTVAR has grown to over 2.3 seconds, decreasing to 2.0
+Nmap scan report for 118.190.174.130
+Host is up (0.050s latency).
+Not shown: 980 closed ports
+PORT      STATE    SERVICE
+22/tcp    open     ssh
+23/tcp    open     telnet
+42/tcp    filtered nameserver
+80/tcp    open     http
+135/tcp   filtered msrpc
+139/tcp   filtered netbios-ssn
+445/tcp   filtered microsoft-ds
+593/tcp   filtered http-rpc-epmap
+3001/tcp  open     nessus
+3301/tcp  open     unknown
+3389/tcp  open     ms-wbt-server
+4444/tcp  filtered krb524
+5800/tcp  filtered vnc-http
+8080/tcp  open     http-proxy
+8081/tcp  open     blackice-icecap
+49152/tcp open     unknown
+49153/tcp open     unknown
+49154/tcp open     unknown
+49155/tcp open     unknown
+49165/tcp open     unknown
+Device type: general purpose
+Running (JUST GUESSING): Microsoft Windows 2012|7|2008|Vista|8.1 (90%)
+OS CPE: cpe:/o:microsoft:windows_server_2012 cpe:/o:microsoft:windows_7::-:professional cpe:/o:microsoft:windows_server_2008:r2 cpe:/o:microsoft:windows_vista::- cpe:/o:microsoft:windows_vista::sp1 cpe:/o:microsoft:windows_8 cpe:/o:microsoft:windows_8.1:r1
+Aggressive OS guesses: Microsoft Windows Server 2012 (90%), Microsoft Windows Server 2012 or Windows Server 2012 R2 (90%), Microsoft Windows Server 2012 R2 (89%), Microsoft Windows 7 Professional (87%), Microsoft Windows Server 2008 R2 (86%), Microsoft Windows 7 SP1 (86%), Microsoft Windows Server 2008 or 2008 Beta 3 (86%), Microsoft Windows 7 (86%), Microsoft Windows 7 SP1 or Windows Server 2008 R2 (86%), Microsoft Windows 7 SP1 or Windows Server 2008 SP2 or 2008 R2 SP1 (86%)
+No exact OS matches for host (test conditions non-ideal).
+Network Distance: 15 hops
+
+OS detection performed. Please report any incorrect results at https://nmap.org/submit/ .
+Nmap done: 1 IP address (1 host up) scanned in 10.92 seconds
+
+```
+
+
+## 相关repo：   
+https://github.com/zizyzhang/unclenoway 创业计划书
+
+## 其他repo：
+https://github.com/handsomeLei/yuelemo
+https://github.com/370917829/app
+
+## java版爬虫：
+https://github.com/chengzequn/CrawlerForShuShuBuYue
+
+## 以上repo的存档
+https://github.com/qwqmoe/app
+https://github.com/qwqmoe/yuelemo
+https://github.com/qwqmoe/unclenoway
+https://github.com/qwqmoe/CrawlerForShuShuBuYue
+
+## 通信
+通信应该是socket  
+疑似  
+https://github.com/zizyzhang/socket.io-client 有po主的commit记录  
+https://github.com/zizyzhang/socket.io
---
+++ unclenoway.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import json
+import requests
+import os
+import time
+import argparse
+import sqlite3
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+db = 'data.db'
+
+def req(number):
+    s = requests.Session()
+    url = 'http://118.190.174.130:3101/lostFound/{}'.format(str(20*number))
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E238 Safari/601.1', 'Content-Type': 'application/x-www-form-urlencoded'}
+    print '[*] payload sending ...'
+    r = s.get(url, headers=headers)
+    
+    data = json.loads(r.text)
+    if not os.path.exists(db):
+        init_db()
+
+    for n in data:
+        _id = n['_id']
+        postNumber = n['postNumber']
+        lostDate = n['lostDate']
+        wechat = n['wechat']
+        title = n['title']
+        article = n['article']
+        question = n['question']
+        posterId = n['posterId']
+        likes = n['likes']
+        postDate = n['postDate']
+        isFound = n['isFound']
+        status = n['status']
+
+        # if 
+        insert(_id, postNumber, lostDate, wechat,title,article,question,posterId,likes,postDate,isFound,status)
+        print 'insert ok, postnumber:{} '.format(postNumber)
+
+
+def init_db():
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    #sqlite3.Warning: You can only execute one statement at a time.
+    SQL_INIT1 = '''PRAGMA foreign_keys = FALSE;'''
+    SQL_INIT2 = '''
+CREATE TABLE IF NOT EXISTS lostFound (
+	_id VARCHAR(2000) NOT NULL,
+	postNumber VARCHAR(20) NOT NULL,
+	lostDate VARCHAR(20) NOT NULL,
+	wechat VARCHAR(2000) NOT NULL,
+	title VARCHAR(2000) NOT NULL,
+    article VARCHAR(2000) NOT NULL,
+    question VARCHAR(2000) NOT NULL,
+    posterId VARCHAR(2000) NOT NULL,
+    likes VARCHAR(2000) NOT NULL,
+    postDate VARCHAR(2000) NOT NULL,
+    isFound VARCHAR(2000) NOT NULL,
+    status VARCHAR(2000) NOT NULL,
+	PRIMARY KEY (postNumber)
+);'''
+    SQL_INIT3 = '''PRAGMA foreign_keys = TRUE;'''
+    
+    cursor.execute(SQL_INIT1)
+    cursor.execute(SQL_INIT2)
+    cursor.execute(SQL_INIT3)
+    print "database init success"
+    cursor.close()
+    conn.commit()
+    conn.close()
+
+# def search(question):
+#     conn = sqlite3.connect('data.db')
+#     sql = "select * from questions where quiz LIKE '{}'"
+#     answer = conn.execute(sql.format(question)).fetchall()[-1][-1]
+#     conn.close()
+#     return answer
+
+
+def insert(_id, postNumber, lostDate, wechat,title,article,question,posterId,likes,postDate,isFound,status):
+    conn = sqlite3.connect('data.db')
+    sql = "INSERT INTO `lostFound`(`_id`, `postNumber`, `lostDate`, `wechat`,`title`,`article`,`question`,`posterId`,`likes`,`postDate`,`isFound`,`status`) VALUES ('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}');"
+    c = conn.cursor()
+    r = c.execute(sql.format(_id, postNumber, lostDate, wechat,title,article,question,posterId,likes,postDate,isFound,status))
+    conn.commit()
+    conn.close()
+
+
+def update(question, answer_no):
+    conn = sqlite3.connect('data.db')
+    sql = "select `options` from `questions` where `quiz` = '{}'"
+    c = conn.cursor()
+    options = c.execute(sql.format(question)).fetchall()[-1][-1]
+    answer = options.strip('[').strip(']').split()[answer_no - 1]
+    sql = "UPDATE `questions` SET `answer`='{}' WHERE `quiz`='{}';"
+    r = c.execute(sql.format(answer, question))
+    conn.commit()
+    conn.close()
+
+
+if __name__ == '__main__':
+    for x in xrange(1967):#39340/88467
+        req(x)
